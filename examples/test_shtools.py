abe13eee54eb99c9b585dd8d80d009750cfadb69
Libby Baker <elba@di.ku.dk>
2022-06-30 11:54:04 +0200

    Tests for shtool library

--- examples/test_shtools.py
+++ examples/test_shtools.py
@@ -4,6 +4,7 @@ import trimesh as tri
 import pyssht
 
 from utils import plotting
+from utils.sphere import Sphere, radians_to_degrees, degrees_to_radians
 
 
 def compute_coefficients(f_1, f_2, f_3, lat, lon, lmax=10):
@@ -18,61 +19,49 @@ def add_noise_to_coefficients(coefficients, standard_deviation=0.1, mean= 0):
     random_vars = np.random.normal(loc=mean, scale=standard_deviation, size=coefficients.shape)
     zeroed_vars = np.where(coefficients==0, 0, random_vars)
     return coefficients + zeroed_vars
-
-class Sphere:
-    def __init__(self, radius=1.0, count=[32, 32]) -> None:
-        self.radius = radius
-        self.mesh = tri.creation.uv_sphere(radius=radius, count=count)
-        self.cartesian_coords = self.mesh.vertices
-        self.polar_coords = tri.util.vector_to_spherical(cartesian=self.cartesian_coords)
         
 
 if __name__ == "__main__":
     sphere = Sphere()
-    phi = sphere.polar_coords[:, 0]
-    theta = sphere.polar_coords[:, 1]
 
-    lat=theta*180/np.pi
-    lon=phi*180/np.pi
+    # lat_deg, lon_deg = sphere._convert_latitudes_and_longitudes()
+    lat_deg, lon_deg = sphere.latitudes, sphere.longitudes
 
-    sample_lat = np.linspace(0, 180, 100)
-    sample_lon = np.linspace(0, 360, 100)
+    x_coeffs = pysh.expand.SHExpandLSQ(d=sphere.points_cartesian[:, 0], lat=lat_deg, lon=lon_deg, lmax=1)
+    y_coeffs = pysh.expand.SHExpandLSQ(d=sphere.points_cartesian[:, 1], lat=lat_deg, lon=lon_deg, lmax=1)
+    z_coeffs = pysh.expand.SHExpandLSQ(d=sphere.points_cartesian[:, 2], lat=lat_deg, lon=lon_deg, lmax=1)
 
-    x_coeffs = pysh.expand.SHExpandLSQ(d=sphere.cartesian_coords[:, 0], lat=lat, lon=lon, lmax=6)
-    y_coeffs = pysh.expand.SHExpandLSQ(d=sphere.cartesian_coords[:, 1], lat=lat, lon=lon, lmax=6)
-    z_coeffs = pysh.expand.SHExpandLSQ(d=sphere.cartesian_coords[:, 2], lat=lat, lon=lon, lmax=6)
+    x_recon = pysh.expand.MakeGridPoint(x_coeffs[0], lat=lat_deg, lon=lon_deg)
+    y_recon = pysh.expand.MakeGridPoint(y_coeffs[0], lat=lat_deg, lon=lon_deg)
+    z_recon = pysh.expand.MakeGridPoint(z_coeffs[0], lat=lat_deg, lon=lon_deg)
 
-    x_recon = pysh.expand.MakeGridPoint(x_coeffs[0], lat=lat, lon=lon)
-    y_recon = pysh.expand.MakeGridPoint(y_coeffs[0], lat=lat, lon=lon)
-    z_recon = pysh.expand.MakeGridPoint(z_coeffs[0], lat=lat, lon=lon)
+    lat_coeffs = pysh.expand.SHExpandLSQ(lat_deg, lat=lat_deg, lon=lon_deg, lmax=1)
+    lon_coeffs = pysh.expand.SHExpandLSQ(lon_deg, lat=lat_deg, lon=lon_deg, lmax=1)
+    r_coeffs = pysh.expand.SHExpandLSQ(np.ones(shape=lat_deg.shape), lat=lat_deg, lon=lon_deg, lmax=1)
 
-    lat_coeffs = pysh.expand.SHExpandLSQ(lat, lat=lat, lon=lon, lmax=6)
-    lon_coeffs = pysh.expand.SHExpandLSQ(lon, lat=lat, lon=lon, lmax=6)
-    r_coeffs = pysh.expand.SHExpandLSQ(np.ones(shape=lat.shape), lat=lat, lon=lon, lmax=6)
+    lat_recon = pysh.expand.MakeGridPoint(lat_coeffs[0], lat=lat_deg, lon=lon_deg)
+    lon_recon = pysh.expand.MakeGridPoint(lon_coeffs[0], lat=lat_deg, lon=lon_deg)
+    r_recon = pysh.expand.MakeGridPoint(r_coeffs[0], lat=lat_deg, lon=lon_deg)
 
-    lat_recon = pysh.expand.MakeGridPoint(lat_coeffs[0], lat=lat, lon=lon)
-    lon_recon = pysh.expand.MakeGridPoint(lon_coeffs[0], lat=lat, lon=lon)
-    r_recon = pysh.expand.MakeGridPoint(r_coeffs[0], lat=lat, lon=lon)
+    # lat_noisy_coeff = add_noise_to_coefficients(lat_coeffs[0])
+    # lon_noisy_coeff = add_noise_to_coefficients(lon_coeffs[0])
+    # r_noisy_coeff = add_noise_to_coefficients(r_coeffs[0])
 
-    lat_noisy_coeff = add_noise_to_coefficients(lat_coeffs[0])
-    lon_noisy_coeff = add_noise_to_coefficients(lon_coeffs[0])
-    r_noisy_coeff = add_noise_to_coefficients(r_coeffs[0])
+    # lat_noisy = pysh.expand.MakeGridPoint(lat_noisy_coeff, lat=lat, lon=lon)
+    # lon_noisy = pysh.expand.MakeGridPoint(lon_noisy_coeff, lat=lat, lon=lon)
+    # r_noisy = pysh.expand.MakeGridPoint(r_noisy_coeff, lat=lat, lon=lon)
 
-    lat_noisy = pysh.expand.MakeGridPoint(lat_noisy_coeff, lat=lat, lon=lon)
-    lon_noisy = pysh.expand.MakeGridPoint(lon_noisy_coeff, lat=lat, lon=lon)
-    r_noisy = pysh.expand.MakeGridPoint(r_noisy_coeff, lat=lat, lon=lon)
-
-    (x, y, z) = pyssht.spherical_to_cart(r_noisy, lat_noisy, lon_noisy)
+    # (x, y, z) = pyssht.spherical_to_cart(r_noisy, lat_noisy, lon_noisy)
 
 
-    plotting.plot_coords(sphere.cartesian_coords[:, 0], sphere.cartesian_coords[:, 1], sphere.cartesian_coords[:, 2], title="Original")
+    plotting.plot_coords(sphere.points_cartesian[:, 0], sphere.points_cartesian[:, 1], sphere.points_cartesian[:, 2], title="Original")
     plotting.plot_coords(x_recon, y_recon, z_recon, title="Reconstructed cart")
     plotting.plot_polar(r= r_recon, phi=lat_recon, theta=lon_recon, title="Reconstructed polar")
-    plotting.plot_coords(x, y, z, "With Noise")
+    # plotting.plot_coords(x, y, z, "With Noise")
 
-    recon_mesh = tri.Trimesh(vertices=np.stack((x, y, z), -1), faces=sphere.mesh.faces)
+    # recon_mesh = tri.Trimesh(vertices=np.stack((x, y, z), -1), faces=sphere.mesh.faces)
 
  #   mesh_recons = tri.convex.convex_hull(np.array([x_coord_recons, y_coord_recons, z_coord_recons]).T)
 
 
-    plotting.plot_mesh(recon_mesh)
+    # plotting.plot_mesh(recon_mesh)
--- tests/test_sphere.py
+++ tests/test_sphere.py
@@ -1,8 +1,69 @@
-from utils.sphere import Sphere
+from math import cos, sin
+import numpy as np
+from utils.sphere import Sphere, radians_to_degrees, degrees_to_radians
 
 def test_points_polar_ordered_same_as_points_cartesian():
     sphere = Sphere()
     polar = sphere.points_polar
     cart = sphere.points_cartesian
 
+    latitude = polar[800][0]
+    longitude = polar[800][1]
+    x = sphere.radius*cos(latitude)*sin(longitude)
+    y = sphere.radius*sin(latitude)*sin(longitude)
+    z = sphere.radius*cos(longitude)
+
+    assert x == cart[800][0]
+    assert y == cart[800][1]
+    assert z == cart[800][2]
+
+def test_latitudes():
+    sphere = Sphere()
+    latitudes = sphere.points_polar[:, 0]
+    assert np.all(-np.pi <= latitudes)
+    assert np.all(latitudes <= np.pi)
+    assert np.any(latitudes <= 0)
+    assert np.any(latitudes>0)
+
+def test_longitude():
+    sphere = Sphere()
+    longitudes = sphere.points_polar[:, 1]
+    assert np.all(0 <= longitudes)
+    assert np.all(longitudes <= np.pi)
+
+def test_radians_to_degrees():
+    radians = np.array([np.pi/2])
+    degrees = np.array([90])
+    assert radians_to_degrees(radians) == degrees
+
+def test_degrees_to_radians():
+    radians = np.array([np.pi/2])
+    degrees = np.array([90])
+    assert degrees_to_radians(degrees) == radians
+
+def test_convert_latitudes_and_longitudes_equivalent():
+    sphere = Sphere()
+    converted_latitudes, converted_longitudes = sphere._convert_latitudes_and_longitudes()
+    x_out, y_out, z_out = polar_to_cartesian(1, np.deg2rad(converted_latitudes),
+                                                 np.deg2rad(converted_longitudes))
+    assert np.allclose(x_out, sphere.points_cartesian[:, 0])
+    assert np.allclose(y_out, sphere.points_cartesian[:, 1])
+    assert np.allclose(z_out, sphere.points_cartesian[:, 2])
+
+def test_convert_latitudes_and_longitudes_range():
+    sphere = Sphere()
+    converted_latitudes, converted_longitudes = sphere._convert_latitudes_and_longitudes()
+    assert np.all(converted_latitudes <= 90)
+    assert np.all(converted_latitudes >= -90)
+    assert np.any(converted_latitudes > 2*np.pi)
+
+    assert np.all(converted_longitudes <= 360)
+    assert np.all(converted_longitudes >= 0)
+    assert np.any(converted_longitudes > 2*np.pi)
     
+
+def polar_to_cartesian(radius, latitudes, longitudes):
+        x = radius*np.cos(latitudes)*np.sin(longitudes)
+        y = radius*np.sin(latitudes)*np.sin(longitudes)
+        z = radius*np.cos(longitudes)
+        return x, y, z
