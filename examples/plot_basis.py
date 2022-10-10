b8885fc08b7c286fbce32ee02f4dbbce815d1c01
tbesnier <tbesnier@LAPTOP-5CI6VPAA.localdomain>
2022-10-10 18:05:08 +0200

    delete some tests

--- examples/plot_basis.py
+++
@@ -1,8 +0,0 @@
-import pyshtools as pysh
-import matplotlib.pyplot as plt
-import numpy as np
-
-basis_fns = lambda theta, phi: pysh.expand.spharm(2, theta, phi)
-
-cilm = np.array([[[1]], [[0]]])
-silm = np.array([[[0]], [[1]]])
